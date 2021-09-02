35522f6e5731a70cb88af1f2f8c1fe4d2bcd758d
betawolfyλ <80059002+betawolfy@users.noreply.github.com>
2021-09-02 10:07:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// actuellement, pour héberger notre bot, nous utilisons REPL.it, ce fichier permet de laisser le bot tourner 24h/24
 const app = require('express')();
 
 app.get('/', (req, res) => res.send('Server is up.'));
