5ba8a46d69454e253939be28b6380363f1480f8b
betawolfyλ <80059002+betawolfy@users.noreply.github.com>
2021-10-02 09:27:20 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const connectDatabase = require("./utils/connectDatabase");
 const express = require("express");
 const router = require("./routes");
 const path = require("path");
+const fs = require("fs");
 
 // Chargement des variables d'environnements.
 require("dotenv").config();
