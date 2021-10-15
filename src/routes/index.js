f97532efa14250bfea622882b49daa78714e9c58
betawolfyλ <80059002+betawolfy@users.noreply.github.com>
2021-10-15 09:47:33 +0200

    petit test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const apiRouter = require("./api");
+const pkg = require("../../package.json"); 
 
 // Ajout des routes pour l'API.
 router.use("/api", apiRouter);
 
 // GET /
 router.get("/", (req, res) => {
-	res.send("Bienvenue sur le site de Botty ! Celui-ci est en cours de construction...");
+	res.send(`Bienvenue sur le site de Botty ! Celui-ci est en cours de construction...\n version du bot: ${pkg.version} `);
 });
 
 module.exports = router;
