c7257edf6a42e9b526fa96a0279809c149f5d07f
betawolfyλ <80059002+betawolfy@users.noreply.github.com>
2021-11-23 08:40:50 +0100

    Create sendpub.js

---
+++ src/commands/sendpub.js
@@ -0,0 +1,25 @@
+module.exports = {
+	data: {
+		name: "sendpub",
+		description: "envoie les pub pour les partenariats",
+		category: "₊˚દ Utilitaires"
+	},
+
+	/**
+	 * @param {import("discord.js").Message} message 
+	 * @param {string[]} args
+	 * @returns {Promise<void>} 
+	 */
+	async execute(message, args) {
+		let textToSay = args.join(" ");
+		if (textToSay.length <= 0) return;
+
+		// On supprime le message original.
+		await message.delete();
+
+		// On envoie celui que dira le bot.
+		await message.channel.send({
+			content:"voici la pub demandé: \n" + textToSay
+		});
+	}
+};
