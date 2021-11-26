2b5809e17c8349dff27fa6c399dfca09ec6db351
betawolfyλ <80059002+betawolfy@users.noreply.github.com>
2021-11-26 11:33:30 +0100

    Update bingo.js

--- src/commands/bingo.js
+++ src/commands/bingo.js
@@ -16,8 +16,8 @@ module.exports = {
 	async execute(message) {
 		
 		const responses = [
-			"**Si les 2 chiffres sont identiques, tu recevras la somme de 100 points pour ton clan et 300 si les 3 sont identiques ! Rends-toi dans ╰・୭︰📮﹒rapports୧﹒ avec une preuve et le staff te donneras le lot (≧▽≦)/.**",
-			"**Si les 2 chiffres sont identiques, tu recevras la somme de 100$ et 600$ si les 3 sont identiques ! Rends-toi dans ╰・୭︰📮﹒rapports୧﹒ avec une preuve et le staff te donneras le lot (≧▽≦)/.**",
+			"**Si les 2 chiffres sont identiques, tu recevras la somme de 100 points pour ton clan et 300 si les 3 sont identiques ! Rends-toi dans <#897416422655926272> avec une preuve et le staff te donneras le lot (≧▽≦)/.**",
+			"**Si les 2 chiffres sont identiques, tu recevras la somme de 100$ et 600$ si les 3 sont identiques ! Rends-toi dans <#897416422655926272> avec une preuve et le staff te donneras le lot (≧▽≦)/.**",
 		];
 		const random = Math.floor((Math.random() * 10) + 1);
 		const random1 = Math.floor((Math.random() * 10) + 1);
